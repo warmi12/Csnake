95d9cbf4746b2c1a23ad4d9b109d408400a64b3b
Maciej Warmijak <mwarmijak98@gmail.com>
2023-07-17 20:56:22 +0200

    initial commit

---
+++ src/main.c
@@ -0,0 +1,29 @@
+#include <ncurses.h>
+
+//what about different files?
+//gif decoder lib in another files
+//src inc
+
+int main(){//args?
+
+
+	//fake loding? no
+	//logo? yes
+	//try to read img or gif and transfer it to ascii gif? yes
+
+
+	//logo later
+	//
+	//its in function
+	//first print options 
+	//new game 
+	//records? high socre?
+	//options
+	//info
+	
+
+	printf("hello\n");
+
+
+
+}
